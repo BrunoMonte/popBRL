5bac9d8ea1f709e22eb8342c615848d0d668b610
BrunoMonte <65718668+BrunoMonte@users.noreply.github.com>
2020-06-14 09:33:07 -0300

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,12 @@
+let pais = 'BRASIL'
+let valor = 250
+let per_mes = 2
+
+if(per_mes == 0){
+    console.log('DIGITE QUANTIDADE DE MESES')
+    
+}else{
+    rend = (valor * 0.2625) / 100 * per_mes
+    total = rend + valor
+    console.log('No país '+pais+' o rendimento do valor R$'+valor+', no período de '+per_mes+' meses, é de R$'+rend+', no total de R$ '+total+'')
+}
